554268de07c46cb8ee575439b5d58766e29de702
savn20 <savn20@student.bth.se>
2021-12-24 09:20:46 +0530

    chore: server setup

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Line based TCP Client
+# TCP Server Client Assignment
+
+## Line based TCP Client
 
 ### Todos
 - [x] Setup connection to reference server `13.53.76.30:5000`  
@@ -11,3 +13,16 @@
 - For floating point use `%8.8g` for formatting
 - Documentation is as important as acutal development.
 - Log Debugging only when the `DEBUG` flag is activated
+
+## Line based TCP Server
+
+### Todos
+- [x] create a server at given `ip` and `port`
+- client management
+    - [ ] handle one client at a time
+    - [ ] 5 clients should be able to queue for service, the sixth should be rejected
+    - [ ] when client gets connected, respond with "TEXT TCP 1.0n"
+- task management
+    - [ ] generate random assignment to client using calcLib
+    - [ ] if the client takes longer than 5s, terminate the client and close the connection
+    - [ ] compare the result from client and server and respond OK if correct, ERROR if wrong
--- servermain.cpp
+++ servermain.cpp
@@ -1,37 +1,123 @@
+#include <iostream>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-/* You will to add includes here */
 
+#include <netdb.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-// Included to get the support library
-#include <calcLib.h>
+// helper library for calculations
+#include "calcLib.h"
 
-// Enable if you want debugging to be printed, see examble below.
-// Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
+// comment the DEBUG macro to turn off comments in the console
 #define DEBUG
-
 
 using namespace std;
 
-
-int main(int argc, char *argv[]){
-  
-  /*
-    Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
-     Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'. 
-  */
-  char delim[]=":";
-  char *Desthost=strtok(argv[1],delim);
-  char *Destport=strtok(NULL,delim);
-  // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
-  // *Dstport points to whatever string came after the delimiter. 
-
-  /* Do magic */
-  int port=atoi(Destport);
-#ifdef DEBUG  
-  printf("Host %s, and port %d.\n",Desthost,port);
+int main(int argc, char *argv[])
+{
+  // disables debugging when there's no DEBUG macro defined
+#ifndef DEBUG
+  cout.setstate(ios_base::failbit);
+  cerr.setstate(ios_base::failbit);
 #endif
 
+  /*
+   * parses command line input to <ip>:<port>
+   * terminates program if there's mismatch
+   */
 
+  if (argc != 2)
+  {
+    cerr << "usage: server <ip>:<port>\n"
+         << "program terminated due to wrong usage" << endl;
+
+    return -1;
+  }
+
+  char seperator[] = ":";
+  string serverIp = strtok(argv[1], seperator);
+  string serverPort = strtok(NULL, seperator);
+
+  int serverSocket = -1, clientSocket = -1, responseBytes = -1;
+
+  sockaddr_in clientAddr;
+  socklen_t clientAddrLen = sizeof(struct sockaddr_in);
+
+  // setting up address metadata
+  addrinfo addressInfo, *serverAddr;
+  memset(&addressInfo, 0, sizeof addressInfo);
+
+  addressInfo.ai_family = AF_INET;
+  addressInfo.ai_socktype = SOCK_STREAM;
+  addressInfo.ai_flags = AI_PASSIVE;
+
+  if (responseBytes = getaddrinfo(serverIp.c_str(), serverPort.c_str(), &addressInfo, &serverAddr) != 0)
+  {
+    cerr << "error: unable to connect to specified host\n"
+         << "program terminated due to host error" << endl;
+
+    return -2;
+  }
+
+  // creating socket
+  if ((serverSocket = socket(serverAddr->ai_family, serverAddr->ai_socktype, serverAddr->ai_protocol)) == -1)
+  {
+    cerr << "error: failed to create socket\n"
+         << "program terminated while creating socket" << endl;
+
+    return -3;
+  }
+
+  // Bind to the set port and IP:
+  if (bind(serverSocket, serverAddr->ai_addr, serverAddr->ai_addrlen) < 0)
+  {
+    cerr << "error: failed to bind socket\n"
+         << "program terminated while binding socket" << endl;
+
+    return -4;
+  }
+
+  freeaddrinfo(serverAddr);
+
+  if (listen(serverSocket, QUEUE) < 0)
+  {
+    cerr << "error: failed to bind socket\n";
+    return -5;
+  }
+
+  cout << "Listening for incoming connections.....";
+
+  //TODO: handle 5 clients
+  if (clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientAddrLen) < 0)
+  {
+    cerr << "error: can't accept client\n";
+    return -6;
+  }
+
+  cout << "client connected at IP: " << inet_ntoa(clientAddr.sin_addr) << "and port: " << ntohs(clientAddr.sin_port) << endl;
+
+  string version = "TEXT TCP 1.0\n\n";
+
+  if (sendto(clientSocket, version.c_str(), strlen(version.c_str()), 0,
+             (struct sockaddr *)NULL, clientAddrLen) < 0)
+  {
+    cout << "can't send" << endl;
+    return -7;
+  }
+
+  char client_message[2000];
+
+  // Receive client's message:
+  if (recv(clientSocket, &client_message, sizeof(client_message), 0) < 0)
+  {
+    cerr << "couldn't receive\n";
+    return -8;
+  }
+
+  cout << "message from client: " << client_message << endl;
+
+  return 0;
 }
